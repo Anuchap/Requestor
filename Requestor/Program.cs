93b29ef83c9f9e342143b6a72f6619cc7612f3b0
anucha8p <anucha8p@gmail.com>
2017-06-29 23:59:02 +0700

    first commit

---
+++ Requestor/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Net;
+
+namespace Requestor
+{
+    internal class Program
+    {
+        private static void Main(string[] args)
+        {
+            try
+            {
+                foreach (var url in args)
+                {
+                    var request = (HttpWebRequest)WebRequest.Create(url);
+                    using (var response = (HttpWebResponse)request.GetResponse())
+                    {
+                        Console.WriteLine(response.StatusCode);
+                    }
+                }
+            }
+            catch (WebException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
